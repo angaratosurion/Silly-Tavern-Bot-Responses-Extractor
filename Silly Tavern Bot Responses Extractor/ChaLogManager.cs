04b23b31755e5f436b96ea5199f7ce2a3c9764f0
angaratosurion <angarato.surion@gmail.com>
2025-08-20 22:06:22 +0300

    .
    now it saved the bot responses to a file

--- Silly Tavern Bot Responses Extractor/ChaLogManager.cs
+++ Silly Tavern Bot Responses Extractor/ChaLogManager.cs
@@ -1,10 +1,11 @@
-﻿using Silly_Tavern_Bot_Responses_Extractor.Models;
+﻿using Newtonsoft.Json;
+using Silly_Tavern_Bot_Responses_Extractor.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Silly_Tavern_Bot_Responses_Extractor
 {
@@ -46,6 +47,88 @@ namespace Silly_Tavern_Bot_Responses_Extractor
 
 				throw;
 			}
+        }
+        public List<Message> GetBotsResponses(List<Message> messages)
+        {
+            try
+            {
+                List<Message> ap = null;
+                if (messages != null)
+                {
+                    //ap = new List<Message>();
+                    ap = messages.FindAll(m => m.IsUser == false).ToList();
+                }
+
+
+
+                return ap;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+        }
+        public string ExportMessagesToText(List<Message> messages,
+            Boolean removeFirstParagraph)
+        {
+            try
+            {
+                string ap= null;
+                if (messages != null)
+                {
+                    //ap = new List<Message>();
+                    foreach (var message in messages)
+                    {
+                        if (removeFirstParagraph  )
+                        {
+                            ap += RemoveFirstParagraph(message.Mes);
+                        }
+                        else
+                        {
+                            ap += message.Mes;
+                        }
+
+                    }
+                }
+
+
+
+                return ap;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        private string RemoveFirstParagraph(string messege)
+        {
+            try
+            {
+                string ap = null;
+                if (messege != null)
+                {
+                    var paragraphs = messege.Split(new string[] { "\n\n",
+                        "\n\n" }, StringSplitOptions.None);
+                    if (paragraphs.Length > 1)
+                    {
+                        string result = string.Join("\n\n", paragraphs[1..]);
+                        ap = result;
+
+                    }
+                }
+
+                return ap;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
--- Silly Tavern Bot Responses Extractor/Program.cs
+++ Silly Tavern Bot Responses Extractor/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Runtime.CompilerServices;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 
@@ -9,7 +11,43 @@ namespace Silly_Tavern_Bot_Responses_Extractor
     {
         static void Main(string[] args)
         {
+            ChaLogManager  chamgr = new ChaLogManager();
+            string inputPath = null;
+            string outputPath = null;
 
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i].ToLower())
+                {
+                    case "--input":
+                        if (i + 1 < args.Length) inputPath = args[i + 1];
+                        i++;
+                        break;
+                    case "--output":
+                        if (i + 1 < args.Length) outputPath = args[i + 1];
+                        i++;
+                        break;
+                }
+            }
+
+            if (string.IsNullOrWhiteSpace(inputPath) || string.IsNullOrWhiteSpace(outputPath))
+            {
+                Console.WriteLine("Usage: SillyTavernExtractor.exe --input <input.jsonl> --output <output.txt>");
+                return;
+            }
+
+            if (!File.Exists(inputPath))
+            {
+                Console.WriteLine($"Input File doesnt exist: {inputPath}");
+                return;
+            }
+            var messaages = chamgr.GetMessages(inputPath);
+           var botMessages= chamgr.GetBotsResponses(messaages);
+           string text = chamgr.ExportMessagesToText(botMessages, true);
+            Console.WriteLine(text);
+            File.WriteAllText(outputPath, text,System.Text.Encoding.UTF8); 
+
+            Console.ReadLine();
         }
     }
 }
